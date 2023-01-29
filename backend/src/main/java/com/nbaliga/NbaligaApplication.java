1a65655daea3ea5bc9063af09debef0a8f4bfa34
Maycon Jardim <mayconsmill@gmail.com>
2023-01-28 23:45:20 -0300

    Backend -> Projeto criado

---
+++ backend/src/main/java/com/nbaliga/NbaligaApplication.java
@@ -0,0 +1,13 @@
+package com.nbaliga;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NbaligaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NbaligaApplication.class, args);
+	}
+
+}
