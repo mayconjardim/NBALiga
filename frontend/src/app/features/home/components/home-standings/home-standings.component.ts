02b1fdff3506c4e3fdf77df3dc6ac02e2905ba4a
Maycon Jardim <mayconsmill@gmail.com>
2023-01-31 16:19:53 -0300

    Frontend -> Team logos, player imagens e imagens utils

--- frontend/src/app/features/home/components/home-standings/home-standings.component.ts
+++ frontend/src/app/features/home/components/home-standings/home-standings.component.ts
@@ -1,5 +1,6 @@
-import { Standings } from './../../models/standings';
 import { Component, Input } from '@angular/core';
+
+import { Standings } from './../../models/standings';
 
 @Component({
   selector: 'home-standings',
@@ -8,4 +9,28 @@ import { Component, Input } from '@angular/core';
 })
 export class HomeStandingsComponent {
   @Input() standings!: Standings[];
+  imgLogo = 'assets/images/logos/';
+
+  displayedColumns: string[] = [
+    'teamName',
+    'wl',
+    'pct',
+    'gb',
+    'pf',
+    'pa',
+    'diff',
+    'strk',
+  ];
+
+  removeLeading(numero: any) {
+    if (numero > 0) {
+      let num = numero;
+      let text = num.toString();
+      text = text.replace(/^[0]+/, '');
+      text = text.slice(0, +4);
+      return text;
+    } else {
+      return '-';
+    }
+  }
 }
--- frontend/src/app/features/home/home.module.ts
+++ frontend/src/app/features/home/home.module.ts
@@ -1,13 +1,21 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+import { MatTableModule } from '@angular/material/table';
 import { RouterModule, Routes } from '@angular/router';
-import { HomeComponent } from './containers/home/home.component';
+
 import { HomeStandingsComponent } from './components/home-standings/home-standings.component';
+import { HomeComponent } from './containers/home/home.component';
 
 export const routes: Routes = [{ path: '', component: HomeComponent }];
 
 @NgModule({
   declarations: [HomeComponent, HomeStandingsComponent],
-  imports: [CommonModule, RouterModule.forChild(routes)],
+  imports: [
+    CommonModule,
+    RouterModule.forChild(routes),
+    MatTableModule,
+    MatIconModule,
+  ],
 })
 export class HomeModule {}
