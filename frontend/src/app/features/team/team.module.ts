30f06f14897ba99386b7b7c7e4577cbf3ff1767f
Maycon Jardim <mayconsmill@gmail.com>
2023-02-01 11:51:47 -0300

    Frontend -> Team Container component

--- frontend/src/app/features/team/team.module.ts
+++ frontend/src/app/features/team/team.module.ts
@@ -1,11 +1,14 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { TeamComponent } from './containers/team/team.component';
 
 export const routes: Routes = [{ path: '' }];
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    TeamComponent
+  ],
   imports: [CommonModule, RouterModule.forChild(routes)],
 })
 export class TeamModule {}
