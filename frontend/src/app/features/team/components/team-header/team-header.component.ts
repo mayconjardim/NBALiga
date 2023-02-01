0e29dc10d996d87f5f5eb630146d114c07ce7596
Maycon Jardim <mayconsmill@gmail.com>
2023-02-01 19:18:18 -0300

    Frontend -> Team header component HTML e CSS

--- frontend/src/app/features/team/components/team-header/team-header.component.ts
+++ frontend/src/app/features/team/components/team-header/team-header.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Team } from './../../models/team';
+import { Component, Input } from '@angular/core';
 
 @Component({
-  selector: 'app-team-header',
+  selector: 'team-header',
   templateUrl: './team-header.component.html',
-  styleUrls: ['./team-header.component.scss']
+  styleUrls: ['./team-header.component.scss'],
 })
 export class TeamHeaderComponent {
+  @Input() team!: Team;
+  logo = 'assets/images/logos/';
 
+  teamColors(team: string) {
+    if (team == '76ers') {
+      return 'Sixers';
+    }
+    return team;
+  }
 }
--- frontend/src/app/features/team/team.module.ts
+++ frontend/src/app/features/team/team.module.ts
@@ -1,13 +1,15 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
 import { RouterModule, Routes } from '@angular/router';
-import { TeamComponent } from './containers/team/team.component';
+
 import { TeamHeaderComponent } from './components/team-header/team-header.component';
+import { TeamComponent } from './containers/team/team.component';
 
 export const routes: Routes = [{ path: '', component: TeamComponent }];
 
 @NgModule({
   declarations: [TeamComponent, TeamHeaderComponent],
-  imports: [CommonModule, RouterModule.forChild(routes)],
+  imports: [CommonModule, RouterModule.forChild(routes), MatCardModule],
 })
 export class TeamModule {}
