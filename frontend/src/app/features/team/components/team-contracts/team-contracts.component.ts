e8dd899c60efbe6022d6bf869db654fc5978f92a
Maycon Jardim <mayconsmill@gmail.com>
2023-02-02 21:06:49 -0300

    Frontend -> Team contracts component

---
+++ frontend/src/app/features/team/components/team-contracts/team-contracts.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-team-contracts',
+  templateUrl: './team-contracts.component.html',
+  styleUrls: ['./team-contracts.component.scss']
+})
+export class TeamContractsComponent {
+
+}
