324611a25aa6c9d01cb206b14c21651b251c9683
Maycon Jardim <mayconsmill@gmail.com>
2023-02-03 20:39:55 -0300

    Backend -> DraftPicks Resource e Service. findAll e findById OK!

---
+++ backend/src/main/java/com/nbaliga/resources/DraftPicksResource.java
@@ -0,0 +1,35 @@
+package com.nbaliga.resources;
+
+import com.nbaliga.dto.DraftPicksDTO;
+import com.nbaliga.services.DraftPicksService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/picks")
+public class DraftPicksResource {
+
+    private final DraftPicksService draftPicksService;
+
+    public DraftPicksResource(DraftPicksService draftPicksService) {
+        this.draftPicksService = draftPicksService;
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<DraftPicksDTO> findById(@PathVariable long id) {
+        DraftPicksDTO dto = draftPicksService.findById(id);
+        return ResponseEntity.ok().body(dto);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<DraftPicksDTO>> findAll() {
+        List<DraftPicksDTO> picks = draftPicksService.findAll();
+        return ResponseEntity.ok().body(picks);
+    }
+}
---
+++ backend/src/main/java/com/nbaliga/services/DraftPicksService.java
@@ -0,0 +1,41 @@
+package com.nbaliga.services;
+
+import com.nbaliga.dto.DraftPicksDTO;
+import com.nbaliga.entities.DraftPicks;
+import com.nbaliga.repositories.DraftPicksRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+@Service
+public class DraftPicksService {
+
+    private final DraftPicksRepository draftPicksRepository;
+
+    public DraftPicksService(DraftPicksRepository draftPicksRepository) {
+        this.draftPicksRepository = draftPicksRepository;
+    }
+
+    @Transactional(readOnly = true)
+    public DraftPicksDTO findById(Long id) {
+        Optional<DraftPicks> obj = draftPicksRepository.findById(id);
+        DraftPicks entity = null;
+        try {
+            entity = obj.orElseThrow( () -> new Exception("Time não encontrado!") );
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+        return new DraftPicksDTO(entity);
+    }
+
+    @Transactional(readOnly = true)
+    public List<DraftPicksDTO> findAll( ) {
+        List<DraftPicks> teams = draftPicksRepository.findAll();
+        return teams.stream().map(x -> new DraftPicksDTO(x)).collect(Collectors.toList());
+    }
+
+}
