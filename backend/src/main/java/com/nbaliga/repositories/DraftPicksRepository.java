55fe0ade8d4d9f5a3ed4457e3d721e2145db71d6
Maycon Jardim <mayconsmill@gmail.com>
2023-02-03 20:35:08 -0300

    Backend -> DraftPicks Repository. findAll customizado

---
+++ backend/src/main/java/com/nbaliga/repositories/DraftPicksRepository.java
@@ -0,0 +1,17 @@
+package com.nbaliga.repositories;
+
+import com.nbaliga.entities.Champs;
+import com.nbaliga.entities.DraftPicks;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface DraftPicksRepository extends JpaRepository<DraftPicks, Long> {
+
+    @Query("SELECT distinct obj FROM DraftPicks obj WHERE obj.year > 0 ORDER by obj.ownedby")
+    List<DraftPicks> findAll();
+
+}
