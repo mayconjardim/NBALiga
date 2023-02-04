ff8ec8d5ceb5dc014547478cfd3663aedbaf2de8
Maycon Jardim <mayconsmill@gmail.com>
2023-02-04 18:14:27 -0300

    Frontend -> Player container component

--- frontend/src/app/features/player/player.module.ts
+++ frontend/src/app/features/player/player.module.ts
@@ -1,11 +1,14 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PlayerComponent } from './containers/player/player.component';
 
 export const routes: Routes = [{ path: '' }];
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    PlayerComponent
+  ],
   imports: [CommonModule, RouterModule.forChild(routes)],
 })
 export class PlayerModule {}
