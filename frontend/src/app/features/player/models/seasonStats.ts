4ad2649715019a5a396a59cffd581bfbd5d7fed8
Maycon Jardim <mayconsmill@gmail.com>
2023-02-04 17:59:38 -0300

    Frontend -> Player e SeasonStats models

---
+++ frontend/src/app/features/player/models/seasonStats.ts
@@ -0,0 +1,37 @@
+export interface SeasonStats {
+  season: number;
+  id: number;
+  minutes: number;
+  league: number;
+  team: string;
+  games: number;
+  gamesStarted: number;
+  mpg: number;
+  fga: number;
+  fgm: number;
+  fgPct: number;
+  threePa: number;
+  threePm: number;
+  threePct: number;
+  fta: number;
+  ftm: number;
+  ftPct: number;
+  offensiveRebounds: number;
+  rebounds: number;
+  rpg: number;
+  assists: number;
+  apg: number;
+  steals: number;
+  spg: number;
+  blocks: number;
+  bpg: number;
+  turnovers: number;
+  tpg: number;
+  points: number;
+  ppg: number;
+  fouls: number;
+  plusMinus: number;
+  dq: number;
+  doubleDoubles: number;
+  tripleDoubles: number;
+}
