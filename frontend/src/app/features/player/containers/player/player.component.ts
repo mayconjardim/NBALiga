2f1e50c89575772539a545fd8e15995c5ec8bae8
Maycon Jardim <mayconsmill@gmail.com>
2023-02-04 18:25:42 -0300

    Frontend -> Player header component

--- frontend/src/app/features/player/containers/player/player.component.ts
+++ frontend/src/app/features/player/containers/player/player.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { PlayerService } from './../../services/player.service';
+import { Component, OnInit } from '@angular/core';
+import { Player } from '../../models/player';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
-  selector: 'app-player',
+  selector: 'player',
   templateUrl: './player.component.html',
-  styleUrls: ['./player.component.scss']
+  styleUrls: ['./player.component.scss'],
 })
-export class PlayerComponent {
+export class PlayerComponent implements OnInit {
+  player!: Player;
 
+  constructor(
+    private route: ActivatedRoute,
+    private playerService: PlayerService
+  ) {}
+
+  ngOnInit(): void {
+    const id = this.route.snapshot.paramMap.get('id');
+    this.playerService.readOne(id).subscribe((player) => {
+      this.player = player;
+    });
+  }
 }
--- frontend/src/app/features/player/player.module.ts
+++ frontend/src/app/features/player/player.module.ts
@@ -2,12 +2,14 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { PlayerComponent } from './containers/player/player.component';
+import { PlayerHeaderComponent } from './components/player-header/player-header.component';
 
 export const routes: Routes = [{ path: '' }];
 
 @NgModule({
   declarations: [
-    PlayerComponent
+    PlayerComponent,
+    PlayerHeaderComponent
   ],
   imports: [CommonModule, RouterModule.forChild(routes)],
 })
