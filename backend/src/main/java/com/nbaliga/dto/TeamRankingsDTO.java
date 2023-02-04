31038d924ee8b2e5742eda2f25c31c6d36fb4db5
Maycon Jardim <mayconsmill@gmail.com>
2023-02-04 16:30:32 -0300

    Backend -> TeamRankingsDTO

--- backend/src/main/java/com/nbaliga/dto/TeamRankingsDTO.java
+++ backend/src/main/java/com/nbaliga/dto/TeamRankingsDTO.java
@@ -1,49 +1,26 @@
 package com.nbaliga.dto;
 
-import com.nbaliga.entities.DraftPicks;
-import com.nbaliga.entities.Player;
 import com.nbaliga.entities.Team;
 import lombok.*;
 
-import javax.persistence.PersistenceContext;
 import java.io.Serializable;
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
 
 @NoArgsConstructor
 @AllArgsConstructor
 @Getter
 @Setter
 @EqualsAndHashCode
-public class TeamDTO implements Serializable {
+public class TeamRankingsDTO implements Serializable {
 
     private Long id;
     private Integer league;
     private String cityName;
     private String name;
     private String city;
-    private String state;
-    private String conference;
-    private String division;
-    private Integer teamRating;
-    private Integer championships;
-    private Integer playoffs;
-    private Integer totalSalary;
-    private Integer numPlayers;
-    private Integer oldWinRating;
-    private Integer streak;
     private Integer homeWins;
     private Integer homeLosses;
     private Integer roadWins;
     private Integer roadLosses;
-    private Double winPct;
-    private Integer totalWins;
-    private Integer totalLosses;
-    private Integer playoffWins;
-    private Integer playoffLosses;
-    private Integer finalsAppearances;
     private Integer points;
     private Integer allowedPoints;
     private Integer steals;
@@ -68,43 +45,17 @@ public class TeamDTO implements Serializable {
     private Integer fta;
     private Integer allowedFtm;
     private Integer allowedFta;
-    private Integer lastPlayoffYear;
-    private Boolean isHuman;
-    private Double gb;
-    private Double confGb;
-    private Integer divRank;
-    private Integer confRank;
-    private String arenaName;
 
-    private List<PlayerDTO> players = new ArrayList<>();
-    private List<DraftPicksDTO> picks = new ArrayList<>();
 
-    public TeamDTO(Team entity) {
+    public TeamRankingsDTO(Team entity) {
         id = entity.getId();
-        league = entity.getLeague();
         cityName = entity.getCityName();
         name = entity.getName();
         city = entity.getCity();
-        state = entity.getState();
-        conference = entity.getConference();
-        division = entity.getDivision();
-        teamRating = entity.getTeamRating();
-        championships = entity.getChampionships();
-        playoffs = entity.getPlayoffs();
-        totalSalary = entity.getTotalSalary();
-        numPlayers = entity.getNumPlayers();
-        oldWinRating = entity.getOldWinRating();
-        streak = entity.getStreak();
         homeWins = entity.getHomeWins();
         homeLosses = entity.getHomeLosses();
         roadWins = entity.getRoadWins();
         roadLosses = entity.getRoadLosses();
-        winPct = entity.getWinPct();
-        totalWins = entity.getTotalWins();
-        totalLosses = entity.getTotalLosses();
-        playoffWins = entity.getPlayoffWins();
-        playoffLosses = entity.getPlayoffLosses();
-        finalsAppearances = entity.getFinalsAppearances();
         points = entity.getPoints();
         allowedPoints = entity.getAllowedPoints();
         steals = entity.getSteals();
@@ -129,19 +80,6 @@ public class TeamDTO implements Serializable {
         fta = entity.getFta();
         allowedFtm = entity.getAllowedFtm();
         allowedFta = entity.getAllowedFta();
-        lastPlayoffYear = entity.getLastPlayoffYear();
-        isHuman = entity.getIsHuman();
-        gb = entity.getGb();
-        confGb = entity.getConfGb();
-        divRank = entity.getDivRank();
-        confRank = entity.getConfRank();
-        arenaName = entity.getArenaName();
-    }
-
-    public TeamDTO(Team entity, List<Player> players, List<DraftPicks> picks) {
-        this(entity);
-        players.forEach(play -> this.players.add(new PlayerDTO(play)));
-        picks.forEach(pick -> this.picks.add(new DraftPicksDTO(pick)));
     }
 
     public String getTeamName() {
@@ -203,14 +141,5 @@ public class TeamDTO implements Serializable {
             return 0.0;
         }
     }
-
-    public String getSTRK() {
-        if (streak > 0) {
-            return "W" +  streak;
-        }
-        return "L" + Math.abs(streak);
-    }
-
-
 
 }
