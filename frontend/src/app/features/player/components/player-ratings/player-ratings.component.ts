2c07c3f44600230712241bdb799ce15c712ad758
Maycon Jardim <mayconsmill@gmail.com>
2023-02-05 18:57:43 -0300

    Frontend -> Player Rating component TS

--- frontend/src/app/features/player/components/player-ratings/player-ratings.component.ts
+++ frontend/src/app/features/player/components/player-ratings/player-ratings.component.ts
@@ -1,8 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit, ViewEncapsulation } from '@angular/core';
+import { Player } from 'src/app/features/player/models/player';
 
 @Component({
   selector: 'player-ratings',
   templateUrl: './player-ratings.component.html',
   styleUrls: ['./player-ratings.component.scss'],
 })
-export class PlayerRatingsComponent {}
+export class PlayerRatingsComponent implements OnInit {
+  @Input() player!: Player;
+  ratings: any[] = [];
+  season = localStorage.getItem('season');
+
+  ngOnInit(): void {
+    this.ratings.push(this.player);
+  }
+
+  displayedColumns: string[] = [
+    'SEASON',
+    'overallRating',
+    'insideScoring',
+    'jumpShot',
+    'freeThrowShot',
+    'threePointShot',
+    'handling',
+    'passing',
+    'offensiveRebounding',
+    'defensiveRebounding',
+    'postDefense',
+    'perimeterDefense',
+    'stealing',
+    'shotBlocking',
+  ];
+}
--- frontend/src/app/features/player/player.module.ts
+++ frontend/src/app/features/player/player.module.ts
@@ -1,16 +1,20 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
 import { RouterModule, Routes } from '@angular/router';
 
 import { PlayerHeaderComponent } from './components/player-header/player-header.component';
-import { PlayerComponent } from './containers/player/player.component';
-import { MatCardModule } from '@angular/material/card';
 import { PlayerRatingsComponent } from './components/player-ratings/player-ratings.component';
+import { PlayerComponent } from './containers/player/player.component';
 
 export const routes: Routes = [{ path: '', component: PlayerComponent }];
 
 @NgModule({
-  declarations: [PlayerComponent, PlayerHeaderComponent, PlayerRatingsComponent],
+  declarations: [
+    PlayerComponent,
+    PlayerHeaderComponent,
+    PlayerRatingsComponent,
+  ],
   imports: [CommonModule, RouterModule.forChild(routes), MatCardModule],
 })
 export class PlayerModule {}
