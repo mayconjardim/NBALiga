96c86910b527625bd03052925736c2eccfa3867d
Maycon Jardim <mayconsmill@gmail.com>
2023-02-05 13:22:26 -0300

    Frontend -> Player Ratings component

---
+++ frontend/src/app/features/player/components/player-ratings/player-ratings.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-player-ratings',
+  templateUrl: './player-ratings.component.html',
+  styleUrls: ['./player-ratings.component.scss']
+})
+export class PlayerRatingsComponent {
+
+}
