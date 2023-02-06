69fbd418776a13ea0c7b8e36eb1b7d49ae0eefe3
Maycon Jardim <mayconsmill@gmail.com>
2023-02-06 14:33:51 -0300

    Backend -> PlayoffsStats Entity e Key

---
+++ backend/src/main/java/com/nbaliga/entities/PlayoffsStats.java
@@ -0,0 +1,69 @@
+package com.nbaliga.entities;
+
+import com.nbaliga.entities.keys.SeasonStatsKey;
+import lombok.*;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.IdClass;
+import java.io.Serializable;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@EqualsAndHashCode
+@Entity
+@IdClass(PlayoffsStats.class)
+public class PlayoffsStats implements Serializable {
+
+    private Integer season;
+    private Long id;
+    @Id
+    private Integer minutes;
+    private Integer league;
+    private String team;
+    private Integer games;
+    private Integer gamesStarted;
+    private Double mpg;
+    private Integer fga;
+    private Integer fgm;
+    @Column(name = "FG_")
+    private Double fgPct;
+    @Column(name = "3PA")
+    private Integer threePa;
+    @Column(name = "3PM")
+    private Integer threePm;
+    @Column(name = "3P_")
+    private Double threePct;
+    private Integer fta;
+    private Integer ftm;
+    @Column(name = "FT_")
+    private Double ftPct;
+    private Integer offensiveRebounds;
+    @Id
+    private Integer rebounds;
+    private Double rpg;
+    @Id
+    private Integer assists;
+    private Double apg;
+    @Id
+    private Integer steals;
+    private Double spg;
+    private Integer blocks;
+    private Double bpg;
+    private Integer turnovers;
+    private Double tpg;
+    @Id
+    private Integer points;
+    private Double ppg;
+    private Integer fouls;
+    private Integer plusMinus;
+    private Integer dq;
+    private Integer doubleDoubles;
+    private Integer tripleDoubles;
+
+
+
+}
---
+++ backend/src/main/java/com/nbaliga/entities/keys/PlayoffsStatsKey.java
@@ -0,0 +1,20 @@
+package com.nbaliga.entities.keys;
+
+import lombok.*;
+
+import java.io.Serializable;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@EqualsAndHashCode
+public class PlayoffsStatsKey implements Serializable {
+
+    private Integer minutes;
+    private Integer rebounds;
+    private Integer assists;
+    private Integer steals;
+    private Integer points;
+
+}
