7705f1f0de3cd9ae3c9f3514abd5590849e722e4
Maycon Jardim <mayconsmill@gmail.com>
2023-02-06 14:34:43 -0300

    Backend -> PlayoffsStats DTO

---
+++ backend/src/main/java/com/nbaliga/dto/PlayoffsStatsDTO.java
@@ -0,0 +1,137 @@
+package com.nbaliga.dto;
+
+import com.nbaliga.entities.PlayoffsStats;
+import com.nbaliga.entities.SeasonStats;
+import lombok.*;
+
+import java.io.Serializable;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@EqualsAndHashCode
+public class PlayoffsStatsDTO implements Serializable {
+
+    private Integer season;
+    private Long id;
+    private Integer minutes;
+    private Integer league;
+    private String team;
+    private Integer games;
+    private Integer gamesStarted;
+    private Double mpg;
+    private Integer fga;
+    private Integer fgm;
+    private Double fgPct;
+    private Integer threePa;
+    private Integer threePm;
+    private Double threePct;
+    private Integer fta;
+    private Integer ftm;
+    private Double ftPct;
+    private Integer offensiveRebounds;
+    private Integer rebounds;
+    private Double rpg;
+    private Integer assists;
+    private Double apg;
+    private Integer steals;
+    private Double spg;
+    private Integer blocks;
+    private Double bpg;
+    private Integer turnovers;
+    private Double tpg;
+    private Integer points;
+    private Double ppg;
+    private Integer fouls;
+    private Integer plusMinus;
+    private Integer dq;
+    private Integer doubleDoubles;
+    private Integer tripleDoubles;
+
+    public PlayoffsStatsDTO(PlayoffsStats entity) {
+        this.id = entity.getId();
+        this.season = entity.getSeason();
+        this.league = entity.getLeague();
+        this.team = entity.getTeam();
+        this.games = entity.getGames();
+        this.gamesStarted = entity.getGamesStarted();
+        this.minutes = entity.getMinutes();
+        this.mpg = entity.getMpg();
+        this.fga = entity.getFga();
+        this.fgm = entity.getFgm();
+        this.fgPct = entity.getFgPct();
+        this.threePa = entity.getThreePa();
+        this.threePm = entity.getThreePm();
+        this.threePct = entity.getThreePct();
+        this.fta = entity.getFta();
+        this.ftm = entity.getFtm();
+        this.ftPct = entity.getFtPct();
+        this.offensiveRebounds = entity.getOffensiveRebounds();
+        this.rebounds = entity.getRebounds();
+        this.rpg = entity.getRpg();
+        this.assists = entity.getAssists();
+        this.apg = entity.getApg();
+        this.steals = entity.getSteals();
+        this.spg = entity.getSpg();
+        this.blocks = entity.getBlocks();
+        this.bpg = entity.getBpg();
+        this.turnovers = entity.getTurnovers();
+        this.tpg = entity.getTpg();
+        this.points = entity.getPoints();
+        this.ppg = entity.getPpg();
+        this.fouls = entity.getFouls();
+        this.plusMinus = entity.getPlusMinus();
+        this.dq = entity.getDq();
+        this.doubleDoubles = entity.getDoubleDoubles();
+        this.tripleDoubles = entity.getTripleDoubles();
+    }
+
+
+    public Double getFGAPg() {
+        return fga.doubleValue() / games;
+    }
+
+    public Double getFGMPg() {
+        return fgm.doubleValue() / games;
+    }
+
+    public Double getThreePAPg() {
+        return threePa.doubleValue() / games;
+    }
+
+    public Double getThreePMPg() {
+        return threePm.doubleValue() / games;
+    }
+
+    public Double getFTAPg() {
+        return fta.doubleValue() / games;
+    }
+
+    public Double getFTMPg() {
+        return ftm.doubleValue() / games;
+    }
+
+    public Double getORebsPg() {
+        return offensiveRebounds.doubleValue() / games;
+    }
+
+    public Double getDRebsPg() {
+        return (rebounds.doubleValue() - offensiveRebounds) / games;
+    }
+
+    public Double getPFouPg() {
+        return fouls.doubleValue() / games;
+    }
+
+    public Double getTOPg() {
+        return turnovers.doubleValue() / games;
+    }
+
+    public Double getPlusMinusPg() {
+        return turnovers.doubleValue() / games;
+    }
+
+
+
+}
