a2a468d176ebc671de20404164a69eb58f0e0eb9
Maycon Jardim <mayconsmill@gmail.com>
2023-02-07 16:23:46 -0300

    Backend -> Awards Repository

---
+++ backend/src/main/java/com/nbaliga/repositories/AwardsRepository.java
@@ -0,0 +1,15 @@
+package com.nbaliga.repositories;
+
+import com.nbaliga.entities.Awards;
+import com.nbaliga.entities.PlayoffStats;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface AwardsRepository extends JpaRepository<Awards, Long> {
+
+
+}
