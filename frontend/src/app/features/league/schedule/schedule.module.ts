f67887c0422d88edd8b32efb7994313052545645
Maycon Jardim <mayconsmill@gmail.com>
2023-02-08 22:29:17 -0300

    Frontend -> Schedule container component

--- frontend/src/app/features/league/schedule/schedule.module.ts
+++ frontend/src/app/features/league/schedule/schedule.module.ts
@@ -1,11 +1,14 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ScheduleComponent } from './containers/schedule/schedule.component';
 
 export const routes: Routes = [{ path: '' }];
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    ScheduleComponent
+  ],
   imports: [CommonModule, RouterModule.forChild(routes)],
 })
 export class ScheduleModule {}
