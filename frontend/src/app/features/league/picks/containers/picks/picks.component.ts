f3855eada1d20d86e0f7248eff842d8a440542a9
Maycon Jardim <mayconsmill@gmail.com>
2023-02-10 15:59:09 -0300

    Frontend -> DraftPicks Container Component

---
+++ frontend/src/app/features/league/picks/containers/picks/picks.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-picks',
+  templateUrl: './picks.component.html',
+  styleUrls: ['./picks.component.scss']
+})
+export class PicksComponent {
+
+}
--- frontend/src/app/features/league/picks/picks.module.ts
+++ frontend/src/app/features/league/picks/picks.module.ts
@@ -1,11 +1,14 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PicksComponent } from './containers/picks/picks.component';
 
 export const routes: Routes = [{ path: '' }];
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    PicksComponent
+  ],
   imports: [CommonModule, RouterModule.forChild(routes)],
 })
 export class DraftPicksModule {}
