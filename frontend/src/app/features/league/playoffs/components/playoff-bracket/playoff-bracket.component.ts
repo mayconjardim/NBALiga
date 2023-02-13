a3d7819889687b8471cabae1c6a3793a57aa9b20
Maycon Jardim <mayconsmill@gmail.com>
2023-02-13 19:51:09 -0300

    Frontend -> Playoff Bracket Component HTML, CSS e TS

--- frontend/src/app/features/league/playoffs/components/playoff-bracket/playoff-bracket.component.ts
+++ frontend/src/app/features/league/playoffs/components/playoff-bracket/playoff-bracket.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { Playoffs } from '../../models/playoffs';
 
 @Component({
@@ -6,6 +6,37 @@ import { Playoffs } from '../../models/playoffs';
   templateUrl: './playoff-bracket.component.html',
   styleUrls: ['./playoff-bracket.component.scss'],
 })
-export class PlayoffBracketComponent {
-  @Input() playoffs!: Playoffs;
+export class PlayoffBracketComponent implements OnInit {
+  @Input() playoffs!: Playoffs[];
+  teamLogo = 'assets/images/logos/';
+
+  firstRound: Playoffs[] = [];
+  secondRound: Playoffs[] = [];
+  conferenceFinals: Playoffs[] = [];
+  finals: Playoffs[] = [];
+
+  ngOnInit(): void {
+    this.getRound();
+  }
+
+  getRound() {
+    this.playoffs.forEach((x) => {
+      if (x.seriesId <= 8) {
+        this.firstRound.push(x);
+      } else if (x.seriesId <= 12) {
+        this.secondRound.push(x);
+      } else if (x.seriesId <= 14) {
+        this.conferenceFinals.push(x);
+      } else {
+        this.finals.push(x);
+      }
+    });
+  }
+
+  teamColors(team: string) {
+    if (team == '76ers') {
+      return 'Sixers';
+    }
+    return team;
+  }
 }
--- frontend/src/app/features/league/playoffs/playoffs.module.ts
+++ frontend/src/app/features/league/playoffs/playoffs.module.ts
@@ -1,19 +1,20 @@
+import { MatIconModule } from '@angular/material/icon';
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatIconModule } from '@angular/material/icon';
-import { MatInputModule } from '@angular/material/input';
-import { MatSelectModule } from '@angular/material/select';
+import { MatCardModule } from '@angular/material/card';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatListModule } from '@angular/material/list';
 import { MatSortModule } from '@angular/material/sort';
 import { MatTableModule } from '@angular/material/table';
 import { RouterModule, Routes } from '@angular/router';
-import { PlayoffstatsComponent } from './containers/playoffstats/playoffstats.component';
+
+import { PlayoffBracketComponent } from './components/playoff-bracket/playoff-bracket.component';
 import { PlayoffstatsListComponent } from './components/playoffstats-list/playoffstats-list.component';
 import { PlayoffsComponent } from './containers/playoffs/playoffs.component';
-import { PlayoffBracketComponent } from './components/playoff-bracket/playoff-bracket.component';
+import { PlayoffstatsComponent } from './containers/playoffstats/playoffstats.component';
 
 export const routes: Routes = [
-  { path: '', component: PlayoffsComponent },
+  { path: 'bracket', component: PlayoffsComponent },
   { path: 'stats', component: PlayoffstatsComponent },
 ];
 
@@ -29,6 +30,10 @@ export const routes: Routes = [
     RouterModule.forChild(routes),
     MatTableModule,
     MatSortModule,
+    MatCardModule,
+    MatListModule,
+    MatDividerModule,
+    MatIconModule,
   ],
 })
 export class PlayoffsModule {}
