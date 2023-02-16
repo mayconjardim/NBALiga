a29f63d4409bd8cd1f2858c85c88a9722d8304ea
Maycon Jardim <mayconsmill@gmail.com>
2023-02-16 16:03:36 -0300

    Backend -> User Resource

---
+++ backend/src/main/java/com/nbaliga/resources/UserResource.java
@@ -0,0 +1,27 @@
+package com.nbaliga.resources;
+
+import com.nbaliga.dto.UserDTO;
+import com.nbaliga.services.UserService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+
+    private final UserService userService;
+
+    public UserResource(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<UserDTO>> findAll() {
+        List<UserDTO> list = userService.findAll();
+        return ResponseEntity.ok().body(list);
+    }
+}
