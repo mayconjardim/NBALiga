650ead3fc4a8aefd3221c2dccc2d4f2f42c0fc55
Maycon Jardim <mayconsmill@gmail.com>
2023-02-20 21:11:42 -0300

    Frontend -> Rotation Container Component

---
+++ frontend/src/app/private/rotation/containers/rotation/rotation.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-rotation',
+  templateUrl: './rotation.component.html',
+  styleUrls: ['./rotation.component.scss']
+})
+export class RotationComponent {
+
+}
--- frontend/src/app/private/rotation/rotation.module.ts
+++ frontend/src/app/private/rotation/rotation.module.ts
@@ -1,11 +1,14 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { RotationComponent } from './containers/rotation/rotation.component';
 
 export const routes: Routes = [{ path: '' }];
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    RotationComponent
+  ],
   imports: [CommonModule, RouterModule.forChild(routes)],
 })
 export class RotationModule {}
