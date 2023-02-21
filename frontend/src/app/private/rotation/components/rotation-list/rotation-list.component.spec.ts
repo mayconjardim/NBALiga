650ca3f648bbb9b5146ebcf0a2f7e8168b9596e1
Maycon Jardim <mayconsmill@gmail.com>
2023-02-20 21:24:49 -0300

    Frontend -> Rotation-List Component

---
+++ frontend/src/app/private/rotation/components/rotation-list/rotation-list.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { RotationListComponent } from './rotation-list.component';
+
+describe('RotationListComponent', () => {
+  let component: RotationListComponent;
+  let fixture: ComponentFixture<RotationListComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ RotationListComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(RotationListComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
--- frontend/src/app/private/rotation/rotation.module.ts
+++ frontend/src/app/private/rotation/rotation.module.ts
@@ -2,12 +2,14 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { RotationComponent } from './containers/rotation/rotation.component';
+import { RotationListComponent } from './components/rotation-list/rotation-list.component';
 
 export const routes: Routes = [{ path: '' }];
 
 @NgModule({
   declarations: [
-    RotationComponent
+    RotationComponent,
+    RotationListComponent
   ],
   imports: [CommonModule, RouterModule.forChild(routes)],
 })
